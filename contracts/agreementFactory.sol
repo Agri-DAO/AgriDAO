8e5c0a91d9110e8c865d94531fc6ac5d1218a605
HMogg <harry.mogg@mycelium.ventures>
2021-12-02 16:37:08 +1000

    Added functionality to agreement factory

--- contracts/agreementFactory.sol
+++ contracts/agreementFactory.sol
@@ -27,15 +27,21 @@ contract AgreementFactory is Ownable {
         uint256 daysSinceCreation;
         uint256 daysOverdue;
         bool loanRepaid;
+        bool ottleyVerified;
+        bool agentVerified;
+        bool loanActive;
     }
 
     //mapping loanNumber to LoanAgreement struct
     mapping(uint256 => LoanAgreement) public _LoanNumberToLoanAgreement;  
 
-    event loanCreated(uint256 Rate, uint256 LoanAmount, uint256 LoanDuration, address AgentAddress, address UserAddress);
+    event loanCreated(uint256 LoanNumber, uint256 Rate, uint256 LoanAmount, uint256 LoanDuration, address AgentAddress, address UserAddress);
     event loanDeprecated(uint256 Rate, uint256 LoanAmount, uint256 LoanDuration, address AgentAddress, address UserAddress, uint256 DaysSinceCreation, 
             uint256 DaysOverdue, bool LoanRepaid);
+    event loanApproved(uint256 LoanNumber, uint256 Rate, uint256 LoanAmount, uint256 LoanDuration, address AgentAddress, address UserAddress, 
+            bool OttleyVerified, bool AgentVerified);
 
+    //will also need to add in {address agentWhitelist, address userWhitelist} into the constructor
     constructor(address OttleyWallet) public {
         loanNumber = 1;
         ottleyWallet = OttleyWallet;
@@ -46,15 +52,49 @@ contract AgreementFactory is Ownable {
         @param LoanAmount amount issued in the loan
         @param LoanDuration duration of the created loan*/
     function createLoanAgreement(uint256 LoanAmount, uint256 LoanDuration, uint256 Rate, address AgentAddress) public returns (uint256) {
+        //require agent address to be on agentWhitelist
+        //require user address to be on userWhitelist
         uint256 newLoanNumber = loanNumber;
         LoanAgreement memory loanAgreement;
-        loanAgreement = LoanAgreement(Rate, 0, LoanAmount, LoanDuration, AgentAddress, msg.sender, 0, 0, false);
+        loanAgreement = LoanAgreement(Rate, 0, LoanAmount, LoanDuration, AgentAddress, msg.sender, 0, 0, false, false, false, false);
         _LoanNumberToLoanAgreement[newLoanNumber] = loanAgreement;
         loanNumber++;
-        //agentWallet must sign
-        //ottleyWallet must sign
-        emit loanCreated(Rate, LoanAmount, LoanDuration, AgentAddress, msg.sender);
+        emit loanCreated(NewLoanNumber, Rate, LoanAmount, LoanDuration, AgentAddress, msg.sender);
         return newLoanNumber;
+    }
+    
+    /** @notice function used to execute an existing loan agreement
+        @param LoanNumber the loan number for the loan to be executed */
+    function ExecuteLoanAgreement(uint256 LoanNumber) public returns (bool) {
+        require(_LoanNumberToLoanAgreement[LoanNumber].ottleyVerified = true, "ERROR: Loan not verified by Ottley Wallet");
+        require(_LoanNumberToLoanAgreement[LoanNumber].agentVerified = true, "ERROR: Loan not verified by agent");
+        _LoanNumberToLoanAgreement[LoaNumber].loanActive = true;
+
+        //fund transfer executes here
+
+        emit loanApproved(LoanNumber, _LoanNumberToLoanAgreement[LoanNumber].rate, _LoanNumberToLoanAgreement[LoanNumber].loanAmount, 
+        _LoanNumberToLoanAgreement[LoanNumber].loanDuration, _LoanNumberToLoanAgreement[LoanNumber].agentAddress, 
+        _LoanNumberToLoanAgreement[LoanNumber].userAddress, _LoanNumberToLoanAgreement[LoanNumber].ottleyVerified, 
+        _LoanNumberToLoanAgreement[LoanNumber].agentVerified);
+        return _LoanNumberToLoanAgreement[LoanNumber].loanActive;
+    }
+
+    /** @notice function call for the Ottley wallet to verify the loan agreement
+        @param LoanNumber the loan number that the verification is for
+        @dev ideally we will be able to remove this function and have this verification performed through ethereum signing*/
+    function OttleyVerify(uint256 LoanNumber) public returns (bool) {
+        require(msg.sender == ottleyWallet, "ERROR: OttleyVerify only callable by Ottley Capital Wallet");
+        _LoanNumberToLoanAgreement[LoanNumber].ottleyVerified = true;
+        return _LoanNumberToLoanAgreement[LoanNumber].ottleyVerified;
+    }
+    /** @notice function call for the Agent to verify the loan agreement
+        @param LoanNumber the loan number that the verification is for
+        @dev ideally we will be able to remove this function and have this verification performed through ethereum signing */
+    function AgentVerify(uint256 LoanNumber) public returns (bool){
+        address AgentWallet = _LoanNumberToLoanAgreement[LoanNumber].agentWallet;
+        require(msg.sender == AgentWallet, "ERROR: AgentVerify only callable by designated Agent wallet");
+        _LoanNumberToLoanAgreement[LoanNumber].agentVerified = true;
+        return _LoanNumberToLoanAgreement[LoanNumber].agentVerified;
     }
 
     /** @notice function call used to rebase all existing loans - to be called by the timekeep oracle
@@ -63,12 +103,14 @@ contract AgreementFactory is Ownable {
     function rebaseAll() public{
 
     }
+
     /** @notice function call used to rebase a specific loan
         @param LoanNumber the LoanNumber of the loan to be rebased*/
     }
     function rebase(uint256 LoanNumber) public {
-        //require(!loanNumberToLoanrepaid[LoanNumber])
+
     }
+
     /** @notice function call used to 'deprecate' a loan - called when a loan has been fully repaid
         @param LoanNumber the loanNumber for the loan to be deprecated */
     function deprecateLoan(uint256 LoanNumber) public{
